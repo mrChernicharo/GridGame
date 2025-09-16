eb9699649de69a0cc4c148084ce935aa386689c1
Felipe Chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2025-09-16 16:54:31 -0300

    trying to get a build

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,10 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
-using Unity.VisualScripting;
-using Unity.VisualScripting.FullSerializer;
-using UnityEditor.SearchService;
 using UnityEngine;
 
 
--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System.ComponentModel;
 public class Gem : MonoBehaviour
 {
     private bool isInitializing = true;
